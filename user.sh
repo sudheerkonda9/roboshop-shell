b9d927163344447a1138507b491e0f1edcd1c4af
sudheerkonda <sudheerkonda9@gmail.com>
2023-10-30 18:49:05 -0400

    Commiting redis

--- user.sh
+++ user.sh
@@ -28,39 +28,39 @@
  }
 
 
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> $LOGFILE
 validate $? "setting up mpm source"
 
- yum install nodejs -y
+ yum install nodejs -y &>> $LOGFILE
 validate $? "Installing Node Js"
 
- useradd roboshop
+ useradd roboshop &>> $LOGFILE
 validate $? "Adding Roboshop User"
 
- mkdir /app
+ mkdir /app &>> $LOGFILE
  validate $? "mkdir app"
 
- curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip
+ curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
   validate $? "downloading catalogue artifact zip file in to /tmp folder"
 
- cd /app 
+ cd /app  &>> $LOGFILE
  validate $? "Moving in to App directory"
- unzip /tmp/user.zip
+ unzip /tmp/user.zip &>> $LOGFILE
   validate $? "UnZipping catalogue.zip in /tmp"
- npm install 
+ npm install  &>> $LOGFILE
  validate $? "NPM dependencies Installation"
- cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+ cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
  validate $? "Copying User.service"
- systemctl daemon-reload
+ systemctl daemon-reload &>> $LOGFILE
 validate $? "daemon reload"
- systemctl enable user 
+ systemctl enable user  &>> $LOGFILE
  validate $? "Start the service by enabling"
- systemctl start user
+ systemctl start user &>> $LOGFILE
  validate $? "Starting catalogue"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 validate $? "Copied mongo.repo into yum.repos.d"
 
- yum install mongodb-org-shell -y
+ yum install mongodb-org-shell -y &>> $LOGFILE
  validate $? "installing mongo Client"
- mongo --host mongodb.joindevops.icu </app/schema/user.js
+ mongo --host mongodb.joindevops.icu </app/schema/user.js &>> $LOGFILE
  validate $? "Loading catalogue data in to mongo db"
